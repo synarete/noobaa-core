ad6ffbd60b33ec6bc27c06260226f4cbec882d76
Shachar Sharon <ssharon@redhat.com>
2021-09-30 10:47:52 +0300

    build: Support build on aarch64
    Tested on AWS/Graviton arm64 machine.
    
    Signed-off-by: Shachar Sharon <ssharon@redhat.com>

--- src/deploy/NVA_build/install_nodejs.sh
+++ src/deploy/NVA_build/install_nodejs.sh
@@ -8,8 +8,19 @@ then
     exit 1
 fi
 
+ARCH=$(uname -m)
+ARCH_TAG=""
+if [[ "$ARCH" == "x86_64" ]]; then
+    ARCH_TAG="x64"
+elif [[ "$ARCH" == "aarch64" ]]; then
+    ARCH_TAG="arm64"
+else
+    echo "unsupported: $ARCH"
+    exit 1
+fi
+
 NODEJS_VERSION=v${NODEJS_VERSION}
-FILE_NAME=node-${NODEJS_VERSION}-linux-x64.tar.xz
+FILE_NAME=node-${NODEJS_VERSION}-linux-${ARCH_TAG}.tar.xz
 NODE_PATH="/usr/local/node"
 
 mkdir -p ${NODE_PATH}
@@ -17,8 +28,8 @@ cd ${NODE_PATH}
 curl -O https://nodejs.org/dist/${NODEJS_VERSION}/${FILE_NAME}
 tar -xf ${FILE_NAME}
 
-ln -s ${NODE_PATH}/node-${NODEJS_VERSION}-linux-x64/bin/node /usr/local/bin/node
-ln -s ${NODE_PATH}/node-${NODEJS_VERSION}-linux-x64/bin/npm /usr/local/bin/npm
-ln -s ${NODE_PATH}/node-${NODEJS_VERSION}-linux-x64/bin/npm /usr/local/bin/npx
+ln -s ${NODE_PATH}/node-${NODEJS_VERSION}-linux-${ARCH_TAG}/bin/node /usr/local/bin/node
+ln -s ${NODE_PATH}/node-${NODEJS_VERSION}-linux-${ARCH_TAG}/bin/npm /usr/local/bin/npm
+ln -s ${NODE_PATH}/node-${NODEJS_VERSION}-linux-${ARCH_TAG}/bin/npm /usr/local/bin/npx
 
 rm ${FILE_NAME} 
